36ffa5c6909adde2d5258b83fd9e86de9c570a08
Catherine Li <catherine.uk.20@gmail.com>
2021-09-01 16:40:14 +0100

    seedinf complete

--- backend/prisma/seed.ts
+++ backend/prisma/seed.ts
@@ -1,4 +1,4 @@
-import { PrismaClient, Role, FlightStatus } from "@prisma/client";
+import { PrismaClient, Role, FlightStatus, TicketStatus } from "@prisma/client";
 import faker from "faker";
 const prisma = new PrismaClient();
 const {
@@ -10,6 +10,27 @@ const {
   getRandomInt,
 } = require("../src/utils/mockData");
 
+const flightStatusArray = [
+  "SCHEDULED",
+  "BOARDING",
+  "CHECKIN",
+  "FINALCALL",
+  "DELAYED",
+  "CANCELLED",
+  "DEPARTED",
+  "ARRIVED",
+];
+
+const ticketStatusArray = [
+  "CONFIRMED",
+  "ONLINECHECKIN",
+  "LUGGAGECHECKIN",
+  "ONBOARD",
+  "CANCELLED",
+];
+
+const classArray = ["econ", "business", "first"];
+
 const getRandomElement = (array: string[]) => {
   const number = Math.floor(Math.random() * array.length);
   return array[number];
@@ -17,7 +38,7 @@ const getRandomElement = (array: string[]) => {
 
 async function seed() {
   const createUserList = [];
-  const numOfUser = getRandomInt(10, 20);
+  const numOfUser = getRandomInt(20, 30);
   for (let i = 0; i < numOfUser; i++) {
     const userData = {
       firstName: faker.name.findName(),
@@ -32,104 +53,134 @@ async function seed() {
     createUserList.push(createdUser);
   }
 
-  const createStaffList = [];
-  const numOfStaff = 5;
-  for (let i = 0; i < numOfStaff; i++) {
-    const staffData = {
-      role: "STAFF" as Role,
-      firstName: faker.name.findName(),
-      lastName: faker.name.lastName(),
-      password: `${getRandomInt(100, 999)}`,
-      email: faker.internet.email(),
-    };
-
-    const createdStaff = await prisma.user.create({
-      data: staffData,
-    });
-    console.log(createdStaff);
-    createStaffList.push(createdStaff);
-  }
-
-  const createdAirportList = [];
-  for (const airportData of airportList) {
-    const createdAirport = await prisma.airport.create({ data: airportData });
-    console.log(createdAirport);
-    createdAirportList.push(createdAirport);
-  }
-
-  const createdAirlineList = [];
-  for (const airlineData of airlineList) {
-    const createdAirline = await prisma.airline.create({ data: airlineData });
-    console.log(createdAirline);
-    createdAirlineList.push(createdAirline);
-  }
-
-  const createdAircraftList = [];
-  for (const aircraftData of aircraftList) {
-    const createdAirCraft = await prisma.aircraft.create({
-      data: aircraftData,
-    });
-    console.log(createdAirCraft);
-    createdAircraftList.push(createdAirCraft);
-  }
-
-  const createdExtraLuggageList = [];
-  for (const extraLuggageData of extraLuggageList) {
-    const createdExtraLuggage = await prisma.extraLuggage.create({
-      data: extraLuggageData,
-    });
-    console.log(createdExtraLuggage);
-    createdExtraLuggageList.push(createdExtraLuggage);
-  }
-  const aircraftIdArray = aircraftList.map((target: any) => target.id);
-
-  const createdFlightNumberList = [];
-  const createdScheduleFlightList = [];
-  const flightStatusArray = [
-    "SCHEDULED",
-    "BOARDING",
-    "CHECKIN",
-    "FINALCALL",
-    "DELAYED",
-    "CANCELLED",
-    "DEPARTED",
-    "ARRIVED",
-  ];
-  for (const flightNumber of flightNumberList) {
-    const { airlineId, ...flightData } = flightNumber;
-    const createdFlightNumber = await prisma.flightNumber.create({
-      data: {
-        ...flightData,
-        airlineId: airlineId,
-        aircraftId: getRandomElement(aircraftIdArray),
-      },
-    });
-    console.log(createdFlightNumber);
-    createdFlightNumberList.push(createdFlightNumber);
-
-    const numOfScheduledFlight = getRandomInt(1, 6);
-
-    for (let i = 0; i < numOfScheduledFlight; i++) {
-      let randomDate = [2021, getRandomInt(10, 11), getRandomInt(10, 31)];
-      let randomTime = [getRandomInt(10, 23), getRandomInt(10, 59)];
-      const scheduleFlightData = {
-        date: parseInt(randomDate.join("")),
-        time: randomTime.join(":"),
-        economicPrice: getRandomInt(0, 40),
-        businessPrice: getRandomInt(100, 300),
-        firstClassPrice: getRandomInt(300, 1000),
-        gateNumber: "A" + getRandomInt(1, 50),
-        flightNumber: { connect: { id: createdFlightNumber.id } },
-        status: getRandomElement(flightStatusArray) as FlightStatus,
+  for (const user of createUserList) {
+    const numOfBooking = getRandomInt(5, 10);
+    for (let i = 0; i < numOfBooking; i++) {
+      const bookingData = {
+        userId: user.id,
+        BookExtraLuggage: {
+          create: [
+            {
+              quantity: getRandomInt(1, 3),
+              ExtraLuggage: { connect: { id: getRandomInt(1, 3) } },
+            },
+          ],
+        },
       };
-
-      const createdScheduleFlight = await prisma.scheduledFlight.create({
-        data: scheduleFlightData,
+      const createdBooking = await prisma.booking.create({
+        data: bookingData,
       });
-      console.log(createdScheduleFlight);
-      createdScheduleFlightList.push(createdScheduleFlight);
+      console.log(createdBooking);
+
+      const numbOfTickets = getRandomInt(1, 5);
+      const randomClass = getRandomElement(classArray);
+      const randomStatus = getRandomElement(ticketStatusArray);
+      const scheduledFlightId = getRandomInt(1, 332);
+      for (let i = 0; i < numbOfTickets; i++) {
+        let ticketData = {
+          class: randomClass,
+          status: randomStatus as TicketStatus,
+          Booking: { connect: { id: createdBooking.id } },
+          scheduledFlight: { connect: { id: scheduledFlightId } },
+        };
+        const createdTicket = await prisma.ticket.create({
+          data: ticketData,
+        });
+
+        console.log(createdTicket);
+      }
     }
   }
+
+  // const createStaffList = [];
+  // const numOfStaff = 5;
+  // for (let i = 0; i < numOfStaff; i++) {
+  //   const staffData = {
+  //     role: "STAFF" as Role,
+  //     firstName: faker.name.findName(),
+  //     lastName: faker.name.lastName(),
+  //     password: `${getRandomInt(100, 999)}`,
+  //     email: faker.internet.email(),
+  //   };
+
+  //   const createdStaff = await prisma.user.create({
+  //     data: staffData,
+  //   });
+  //   console.log(createdStaff);
+  //   createStaffList.push(createdStaff);
+  // }
+
+  // const createdAirportList = [];
+  // for (const airportData of airportList) {
+  //   const createdAirport = await prisma.airport.create({ data: airportData });
+  //   console.log(createdAirport);
+  //   createdAirportList.push(createdAirport);
+  // }
+
+  // const createdAirlineList = [];
+  // for (const airlineData of airlineList) {
+  //   const createdAirline = await prisma.airline.create({ data: airlineData });
+  //   console.log(createdAirline);
+  //   createdAirlineList.push(createdAirline);
+  // }
+
+  // const createdAircraftList = [];
+  // for (const aircraftData of aircraftList) {
+  //   const createdAirCraft = await prisma.aircraft.create({
+  //     data: aircraftData,
+  //   });
+  //   console.log(createdAirCraft);
+  //   createdAircraftList.push(createdAirCraft);
+  // }
+
+  // const createdExtraLuggageList = [];
+  // for (const extraLuggageData of extraLuggageList) {
+  //   const createdExtraLuggage = await prisma.extraLuggage.create({
+  //     data: extraLuggageData,
+  //   });
+  //   console.log(createdExtraLuggage);
+  //   createdExtraLuggageList.push(createdExtraLuggage);
+  // }
+  // const aircraftIdArray = aircraftList.map((target: any) => target.id);
+
+  // const createdFlightNumberList = [];
+  // const createdScheduleFlightList = [];
+
+  // for (const flightNumber of flightNumberList) {
+  //   const { airlineId, ...flightData } = flightNumber;
+  //   const createdFlightNumber = await prisma.flightNumber.create({
+  //     data: {
+  //       ...flightData,
+  //       airlineId: airlineId,
+  //       aircraftId: getRandomElement(aircraftIdArray),
+  //     },
+  //   });
+  //   console.log(createdFlightNumber);
+  //   createdFlightNumberList.push(createdFlightNumber);
+
+  //   const numOfScheduledFlight = getRandomInt(1, 6);
+
+  //   for (let i = 0; i < numOfScheduledFlight; i++) {
+  //     let randomDate = [2021, getRandomInt(10, 11), getRandomInt(10, 31)];
+  //     let randomTime = [getRandomInt(10, 23), getRandomInt(10, 59)];
+  //     const scheduleFlightData = {
+  //       date: parseInt(randomDate.join("")),
+  //       time: randomTime.join(":"),
+  //       economicPrice: getRandomInt(0, 40),
+  //       businessPrice: getRandomInt(100, 300),
+  //       firstClassPrice: getRandomInt(300, 1000),
+  //       gateNumber: "A" + getRandomInt(1, 50),
+  //       flightNumber: { connect: { id: createdFlightNumber.id } },
+  //       status: getRandomElement(flightStatusArray) as FlightStatus,
+  //     };
+
+  //     const createdScheduleFlight = await prisma.scheduledFlight.create({
+  //       data: scheduleFlightData,
+  //     });
+  //     console.log(createdScheduleFlight);
+  //     createdScheduleFlightList.push(createdScheduleFlight);
+  //   }
+  // }
 }
 
 seed()
