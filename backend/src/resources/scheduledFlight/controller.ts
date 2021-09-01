bb2c0eef204a9af4f8869e00368b4d09ae590a9c
Catherine Li <catherine.uk.20@gmail.com>
2021-09-01 13:46:44 +0100

    ssetup scheudledFlight router

---
+++ backend/src/resources/scheduledFlight/controller.ts
@@ -0,0 +1,4 @@
+import { Request, Response } from "express";
+import dbClient from "../../utils/database";
+
+const { scheduledFlight } = dbClient;
---
+++ backend/src/resources/scheduledFlight/router.ts
@@ -0,0 +1,6 @@
+import { Router } from "express";
+// import {} from "./controller";
+
+const router = Router();
+
+export default router;
