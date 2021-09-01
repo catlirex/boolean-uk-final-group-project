7a6ee69c2652128bca3b079cfc97f9d0dfc02733
Catherine Li <catherine.uk.20@gmail.com>
2021-09-01 17:11:25 +0100

    booking get by id & userid done

--- backend/dist/app.js
+++ backend/dist/app.js
@@ -4,30 +4,32 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
 const router_1 = __importDefault(require("./resources/airport/router"));
 const router_2 = __importDefault(require("./resources/user/router"));
 const router_3 = __importDefault(require("./resources/auth/router"));
 const router_4 = __importDefault(require("./resources/scheduledFlight/router"));
+const router_5 = __importDefault(require("./resources/booking/router"));
 // App initialisation
 const app = (0, express_1.default)();
 // MiddleWares
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express_1.default.json());
 app.use(express_1.default.urlencoded({ extended: false }));
 app.use(cookieParser());
 // Auth
 app.use(router_3.default);
 // Airport
-app.use('/airports', router_1.default);
+app.use("/airports", router_1.default);
 //User
 app.use("/users", router_2.default);
 //Scheduled Flight
 app.use("/scheduledFlight", router_4.default);
+app.use("/bookings", router_5.default);
 // Catch All
-app.all('*', (req, res) => {
-    res.json({ msg: 'ok' });
+app.all("*", (req, res) => {
+    res.json({ msg: "ok" });
 });
 module.exports = app;
 //# sourceMappingURL=app.js.map
--- backend/dist/resources/booking/router.js
+++ backend/dist/resources/booking/router.js
@@ -1,2 +1,9 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = require("express");
+const conroller_1 = require("./conroller");
+const router = (0, express_1.Router)();
+router.get("/user/:userId", conroller_1.getBookingForOneUser);
+router.get("/:id", conroller_1.getOneBookingDetail);
+exports.default = router;
 //# sourceMappingURL=router.js.map
