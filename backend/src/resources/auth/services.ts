656eb066841cf61e85409f6f077d9586e0501f31
Tijani Fulani <Fulani07@gmail.com>
2021-09-01 11:40:59 +0100

    auth services

---
+++ backend/src/resources/auth/services.ts
@@ -0,0 +1,27 @@
+import dbClient from '../../utils/database';
+import { User } from '.prisma/client';
+import { compare, hash } from 'bcrypt';
+
+export const findUserWithValidation = async (userData: User) => {
+  const foundUser = await dbClient.user.findUnique({
+    where: { username: userData.firstName },
+  });
+
+  if (!foundUser) throw new Error('Username/Password incorrect');
+
+  const isPasswordValid = await compare(userData.password, foundUser.password);
+  if (!isPasswordValid) throw new Error('Username/Password incorrect');
+
+  return foundUser;
+};
+
+export const createdWithHash = async (newUser: User) => {
+  const plainText = newUser.password;
+
+  const hashedPassword = await hash(plainText, 15);
+  const savedUser = dbClient.user.create({
+    data: { ...newUser, password: hashedPassword },
+  });
+
+  return savedUser;
+};
